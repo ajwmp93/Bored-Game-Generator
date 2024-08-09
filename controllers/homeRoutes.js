6ceb2a5efe79bfca3211bbe0bd5eca88c85f32df
defrak2 <defranciscok22@gmail.com>
2024-08-08 21:22:12 -0400

    as a group developed routes and favorites route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,38 +4,26 @@ const router = require('express').Router();
 
 // home, login, individual project
 router.get('/', async (req, res) => {
-    const userData = await User.findByPk(1, {
-      include: Games
-    });
-    console.log(userData)
-    const user = userData.get({plain: true})
-    console.log(user)
-    res.render('homepage',  { user }) ;
+
+    res.render('homepage') ;
 })
 
 router.get('/login', (req, res) => {
   res.render('login');
 })
 
-router.get('/favorites', (req, res) => {
-  res.render('favorites')
+router.get('/favorites', async (req, res) => {
+  const userData = await User.findByPk(1, {
+    include: Games
+  });
+  console.log(userData)
+  const user = userData.get({plain: true})
+  console.log(user)
+  res.render('favorites',  { user })
 })
 
-router.get('/spoopy', (req, res) => {
-  res.render('spoopy')
-})
-
-
-router.get('/sus', (req, res) => {
-  res.render('sus')
-})
-
-router.get('/heroic', (req, res) => {
-  res.render('heroic')
-})
-
-router.get('/survival', (req, res) => {
-  res.render('survival')
+router.get('/moods', (req, res) => {
+  res.render('moods')
 })
 
 
