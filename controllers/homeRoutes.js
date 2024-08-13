299b0b7e8bfd9223659784f3ea75c80e5ee91996
Ashley Wright <ashleywright@iMac.local>
2024-08-12 22:15:12 -0400

    as a group, we worked on game, moods, and game moods tables and are starting to get the seeds files filled in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,22 +22,25 @@ router.get('/favorites', async (req, res) => {
   res.render('favorites',  { user })
 })
 
-router.get('/moods', (req, res) => {
-  res.render('moods')
-})
+router.get('/moods', async (req, res) => {
 
+  console.log('moods route')
+  try {
+    const selectedMood = req.query.mood;
 
+    // Query the database for games with the selected mood
+    const games = await Games.findAll({
+      where: {
+        mood: selectedMood,
+      },
+    });
+console.log(games)
+    // Render the view with the list of games and the selected mood
+    res.render('moods', { games: games.map(game => game.toJSON()), mood: selectedMood });
+  } catch (err) {
+    console.error('Error fetching games:', err);
+    res.status(500).json({ message: 'An error occurred while fetching games' });
+  }
+});
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
---
+++ models/GameMoods.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class GameMoods extends Model {}
+
+GameMoods.init(
+  {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'game',
+        key: 'id',
+      },
+    },
+    mood_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'mood',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'gameMoods',
+  }
+);
+
+module.exports = GameMoods;
