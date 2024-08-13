6c66ebb3e9865fffce9ae5fb5926b97be7bd619a
defrak2 <defranciscok22@gmail.com>
2024-08-12 21:04:25 -0400

    added mood page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,43 +1,51 @@
-const { Games, User } = require('../models');
+const express = require('express');
+const router = express.Router();
+const { Games, User } = require('../models'); 
 
-const router = require('express').Router();
-
-// home, login, individual project
+// Home route
 router.get('/', async (req, res) => {
+  res.render('homepage');
+});
 
-    res.render('homepage') ;
-})
-
+// Login route
 router.get('/login', (req, res) => {
   res.render('login');
-})
+});
 
+// Favorites route
 router.get('/favorites', async (req, res) => {
-  const userData = await User.findByPk(1, {
-    include: Games
-  });
-  console.log(userData)
-  const user = userData.get({plain: true})
-  console.log(user)
-  res.render('favorites',  { user })
-})
+  try {
+    const userData = await User.findByPk(1, {
+      include: Games
+    });
+    const user = userData.get({ plain: true });
+    res.render('favorites', { user });
+  } catch (err) {
+    console.error('Error fetching favorites:', err);
+    res.status(500).json({ message: 'An error occurred while fetching favorites' });
+  }
+});
 
-router.get('/moods', (req, res) => {
-  res.render('moods')
-})
+// Route to list all available moods (if needed)
+router.get('/moods', async (req, res) => {
 
+  console.log('moods route')
+  try {
+    const selectedMood = req.query.mood;
 
+    // Query the database for games with the selected mood
+    const games = await Games.findAll({
+      where: {
+        mood: selectedMood,
+      },
+    });
+console.log(games)
+    // Render the view with the list of games and the selected mood
+    res.render('moods', { games: games.map(game => game.toJSON()), mood: selectedMood });
+  } catch (err) {
+    console.error('Error fetching games:', err);
+    res.status(500).json({ message: 'An error occurred while fetching games' });
+  }
+});
 
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
